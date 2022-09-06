9f63df4a213fb99641cb2404a551abcc7bc08fea
Laura Munsch <laura.munsch@neuf.fr>
2022-09-06 19:53:02 +0200

    generate User resource with 3 users in seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+
+user1 = User.create(name: "Carlson", firstname: "Anne", phone: 684539203)
+user2 = User.create(name: "Foster", firstname: "Nathan", phone: 722487490)
+user1 = User.create(name: "Koyne", firstname: "Frankie", phone: 667884291)
