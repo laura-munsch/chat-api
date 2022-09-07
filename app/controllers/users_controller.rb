cc8d11bb21d78e4ded2165511c1923e037d6ebbc
Laura Munsch <laura.munsch@neuf.fr>
2022-09-07 21:02:51 +0200

    create controllers for Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+  def index
+    @Users = User.all
+
+    render json: @Users
+  end
+
+  def show
+    @User = User.find(params[:id])
+
+    render json: @User
+  end
+
+  def create
+    @User = User.create(
+      name: params[:name],
+      firstname: params[:firstname],
+      phone: params[:phone]
+    )
+
+    render json: @User
+  end
 end
