f622a09fe7439b3221abbb207a0c470e360c613c
Laura Munsch <laura.munsch@neuf.fr>
2022-09-12 16:54:01 +0200

    remove routes not used in the Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users 
+  resources :users, only: [:index, :show, :create]
   get '/messages/by-users/:user1/:user2', to: 'messages#indexByUsers'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
